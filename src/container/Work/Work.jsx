c57378d22d7ae58329cec66f9772caeefe70a7d0
Nicolò Teseo <teseonicolo@gmail.com>
2022-12-04 19:23:38 +0100

    Update Work.jsx

--- src/container/Work/Work.jsx
+++ src/container/Work/Work.jsx
@@ -41,7 +41,7 @@ const Work = () => {
       <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
 
       <div className="app__work-filter">
-        {['UI/UX', 'Web App', 'Ecommerce', 'Vue.js', 'All'].map((item, index) => (
+        {['UI/UX', 'Web App', 'Ecommerce', 'React', 'All'].map((item, index) => (
           <div
             key={index}
             onClick={() => handleWorkFilter(item)}
